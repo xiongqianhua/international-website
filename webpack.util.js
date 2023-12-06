e98ff3de45e81b5e5d3f0f4850bac7b0e0752639
user <user@MacBook-Pro.local>
2023-12-06 20:42:02 +0800

    vue3多页面应用

---
+++ webpack.util.js
@@ -0,0 +1,50 @@
+/*
+ * @Author: qianhua.xiong
+ */
+const glob = require('glob');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function setEntry() {
+  const files = glob.sync('./src/pages/**/index.js')
+  const entry = {}
+  files.forEach(file => {
+    const ret = file.match(/^\.\/src\/pages\/(\S*)\/index\.js$/)
+    if (ret) {
+      entry[ret[1]] = {
+        import: file,
+        dependOn: 'vue_vendors',
+      }
+    }
+  })
+   // 拆分vue依赖
+   entry['vue_vendors'] = {
+    import: ['vue'],
+    filename: '_commom/[name].js'
+  }
+  return entry
+}
+function getTemplate() {
+    const files = glob.sync(`./src/index.html`)
+    return files[0]
+}
+  
+function setHtmlPlugin() {
+    const files = glob.sync('./src/pages/**/index.js')
+    const options = []
+    files.forEach(file => {
+      const ret = file.match(/^\.\/src\/pages\/(\S*)\/index\.js$/)
+      if (ret) {
+        const name = ret[1]
+        options.push(new HtmlWebpackPlugin({
+          filename: name === 'main'?'index.html' :`${name}.html`,
+          template: getTemplate(),
+          chunks: ['vue_vendors', name, '[name]/index.css']
+        }))
+      }
+    })
+    return options
+  }
+module.exports = {
+  setEntry,
+  setHtmlPlugin
+}
